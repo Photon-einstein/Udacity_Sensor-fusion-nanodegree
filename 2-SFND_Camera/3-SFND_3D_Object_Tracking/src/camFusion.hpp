5624c7311325ddbd85ec31e192b4559f73383843
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2023-12-27 23:44:37 +0000

    Add files via upload

---
+++ 2-SFND_Camera/3-SFND_3D_Object_Tracking/src/camFusion.hpp
@@ -0,0 +1,33 @@
+
+#ifndef camFusion_hpp
+#define camFusion_hpp
+
+#include <stdio.h>
+#include <vector>
+#include <opencv2/core.hpp>
+#include "dataStructures.h"
+
+typedef struct {
+    int idBoudingBoxMax=-1;
+    int maxKeypointsInBoundingBox=0;
+} boundingBoxCompare;
+
+void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<LidarPoint> &lidarPoints, float shrinkFactor, cv::Mat &P_rect_xx, cv::Mat &R_rect_xx, cv::Mat &RT);
+void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, std::vector<cv::DMatch> &kptMatches);
+void matchBoundingBoxes(std::vector<cv::DMatch> &matches, std::map<int, int> &bbBestMatches, DataFrame &prevFrame, DataFrame &currFrame);
+
+void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, cv::Size imageSize, bool bWait=true);
+
+void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr,
+                      std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC, cv::Mat *visImg=nullptr);
+void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
+                     std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC);
+
+// auxiliary methods
+void testBoundingBoxAgainstKeypoint(const cv::KeyPoint &keypoint, std::vector<BoundingBox> &boundingBoxes, std::vector<int> &boundingBoxesMatchesId);
+
+void updateLidarXVectors(const std::vector<LidarPoint> &v, std::vector<double> &vX, const double laneWidth);
+
+double medianVectorCalc(std::vector<double> &v);
+
+#endif /* camFusion_hpp */
